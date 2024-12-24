c0f52af4579808339bc57320f9e23df7be63077b
Dzacky Ilham <dzackyilham04@gmail.com>
2024-12-24 12:48:50 +0800

    feat : route initialization

--- app/src/main/java/com/example/musically/ui/navigation/Screen.kt
+++ app/src/main/java/com/example/musically/ui/navigation/Screen.kt
@@ -1,4 +1,10 @@
 package com.example.musically.ui.navigation
 
-class Screen {
+sealed class Screen(val route: String) {
+    object Home : Screen("home")
+    object Profile : Screen("profile")
+    object Favorite : Screen ("favorite")
+    object Detail : Screen("detail/{musicallyId}") {
+        fun createRoute(musicallyId: Long) = "detail/$musicallyId"
+    }
 }
