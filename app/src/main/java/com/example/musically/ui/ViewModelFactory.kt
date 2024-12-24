85f5df07e4bbc290cf65875dd299cb2eb7fe970f
Dzacky Ilham <dzackyilham04@gmail.com>
2024-12-24 12:49:15 +0800

    feat : factory initialization

--- app/src/main/java/com/example/musically/ui/ViewModelFactory.kt
+++ app/src/main/java/com/example/musically/ui/ViewModelFactory.kt
@@ -1,4 +1,24 @@
 package com.example.musically.ui
 
-class ViewModelFactory {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.musically.data.MusicallyRepository
+import com.example.musically.ui.screen.detail.DetailViewModel
+import com.example.musically.ui.screen.favorite.FavoriteViewModel
+import com.example.musically.ui.screen.home.HomeViewModel
+
+class ViewModelFactory(private val repository: MusicallyRepository) :
+    ViewModelProvider.NewInstanceFactory() {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(HomeViewModel::class.java)) {
+            return HomeViewModel(repository) as T
+        } else if (modelClass.isAssignableFrom(FavoriteViewModel::class.java)) {
+            return FavoriteViewModel(repository) as T
+        } else if (modelClass.isAssignableFrom(DetailViewModel::class.java)) {
+            return DetailViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class: " + modelClass.name)
+    }
 }
