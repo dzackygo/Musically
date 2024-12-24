76caa4a770ddecddf05e194444c3ab214a68909f
Dzacky Ilham <dzackyilham04@gmail.com>
2024-12-24 12:54:27 +0800

    feat : make repository

--- app/src/main/java/com/example/musically/data/MusicallyRepository.kt
+++ app/src/main/java/com/example/musically/data/MusicallyRepository.kt
@@ -1,7 +1,50 @@
 package com.example.musically.data
 
-class MusicallyRepository {
+import com.example.musically.model.FakeMusically
+import com.example.musically.model.FakeMusicallyDataSource
+import com.example.musically.model.Musically
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.map
 
+class MusicallyRepository {
+    private val favoriteMusic = mutableListOf<FakeMusically>()
+
+    init {
+        if (favoriteMusic.isEmpty()) {
+            FakeMusicallyDataSource.dataDummyMusicfy.forEach { music ->
+                favoriteMusic.add(
+                    FakeMusically(
+                        musically = music,
+                        count = 1,
+                        isFavorite = false
+                    )
+                )
+            }
+        }
+    }
+
+    fun getAllMusically(): Flow<List<FakeMusically>> {
+        return flowOf(favoriteMusic)
+    }
+
+    fun getMusicallyById(musicallyId: Long): FakeMusically {
+        return favoriteMusic.first {
+            it.musically.id == musicallyId
+        }
+    }
+
+    fun updateFavoriteMusically(musicallyId: Long) {
+        val index = favoriteMusic.indexOfFirst { it.musically.id == musicallyId }
+        if (index >= 0) {
+            val item = favoriteMusic[index]
+            favoriteMusic[index] = item.copy(isFavorite = !item.isFavorite)
+        }
+    }
+
+    fun getFavoriteMusically(): Flow<List<FakeMusically>> {
+        return flowOf(favoriteMusic.filter { it.isFavorite })
+    }
 
     companion object {
         @Volatile
@@ -9,9 +52,7 @@ class MusicallyRepository {
 
         fun getInstance(): MusicallyRepository =
             instance ?: synchronized(this) {
-                MusicallyRepository().apply {
-                    instance = this
-                }
+                instance ?: MusicallyRepository().also { instance = it }
             }
     }
 }
