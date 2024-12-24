bf18a8671152aee9b872d0511772266f884f2d0a
Dzacky Ilham <dzackyilham04@gmail.com>
2024-12-24 12:50:08 +0800

    feat : make components

--- app/src/main/java/com/example/musically/ui/component/MusicallyItem.kt
+++ app/src/main/java/com/example/musically/ui/component/MusicallyItem.kt
@@ -1,4 +1,77 @@
 package com.example.musicfy.ui.component
 
-class MusicallyItem {
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.musically.R
+import com.example.musically.model.Musically
+
+@Composable
+fun MusicallyItem(
+    musically: Musically,
+    modifier: Modifier = Modifier,
+) {
+    Card(
+        shape = RoundedCornerShape(8.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surfaceContainer
+        ),
+        modifier = modifier.fillMaxWidth()
+            .padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row {
+            Column(modifier = Modifier.padding(8.dp)) {
+                Image(
+                    painter = painterResource(musically.image),
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .height(120.dp)
+                )
+            }
+            Column(modifier = Modifier.padding(8.dp)) {
+                Row {
+                    Text(
+                        text = musically.title,
+                        maxLines = 2,
+                        style = MaterialTheme.typography.titleMedium
+                    )
+                }
+                Row {
+                    Text(
+                        text = musically.singers
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true, device = Devices.PIXEL_4)
+@Preview(showBackground = true, device = Devices.PIXEL_4, uiMode = UI_MODE_NIGHT_YES)
+fun MusicallyItemPreview() {
+    MaterialTheme {
+        MusicallyItem(
+            musically = Musically(2,"Sleep Well",
+                "d4vd", R.drawable.d4vd, "30.964.242 Pendengar", "Singer and songwriter d4vd (pronounced David) is known for his moody, bedroom-style indie pop and R&B."),
+            modifier = Modifier.padding(8.dp)
+        )
+    }
 }
--- app/src/main/java/com/example/musically/ui/component/TopBar.kt
+++ app/src/main/java/com/example/musically/ui/component/TopBar.kt
@@ -1,4 +1,52 @@
 package com.example.musicfy.ui.component
 
-class TopBar {
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.musically.ui.theme.MusicallyTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBarBack(
+    navController: NavHostController,
+    text: String,
+    modifier: Modifier = Modifier
+) {
+    TopAppBar(
+        title = {
+            Text(text = text)
+        },
+        navigationIcon = {
+            IconButton(onClick = {
+                navController.navigateUp()
+            }) {
+                Icon(
+                    imageVector = Icons.Default.ArrowBack,
+                    contentDescription = "arrow_back"
+                )
+            }
+        }
+    )
+}
+
+@Composable
+@Preview(showBackground = true, device = Devices.PIXEL_4)
+@Preview(showBackground = true, device = Devices.PIXEL_4, uiMode = UI_MODE_NIGHT_YES)
+fun TopBarBackPreview() {
+    MusicallyTheme {
+        val navController = rememberNavController()
+        TopBarBack(navController, "TopBarBack")
+    }
 }
